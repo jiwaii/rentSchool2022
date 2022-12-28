1943b00e05780a781bb0651d618496eb60136b55
jiwaii <jeanyves.laurent@promsocatc.net>
2022-12-28 18:38:36 +0100

    mise en place des pages

--- src/main/java/be/jyl/managedBeans/UserBean.java
+++ src/main/java/be/jyl/managedBeans/UserBean.java
@@ -1,12 +1,21 @@
 package be.jyl.managedBeans;
 import be.jyl.entities.UsersEntity;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
 @Named
-@SessionScoped
+@ApplicationScoped
 public class UserBean implements Serializable {
+    private String message = "Hello La compagnie Créole";
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
